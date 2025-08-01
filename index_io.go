06e837991988a0df435253092b0b0ae298f4fc28
Fan Cui <cuifaneo@gmail.com>
2025-08-01 11:45:31 +0800

    distributed io implemented

--- index_io.go
+++ index_io.go
@@ -7,6 +7,12 @@ package faiss
 import "C"
 import "unsafe"
 
+// IO flags
+const (
+	IOFlagMmap     = C.FAISS_IO_FLAG_MMAP
+	IOFlagReadOnly = C.FAISS_IO_FLAG_READ_ONLY
+)
+
 // WriteIndex writes an index to a file.
 func WriteIndex(idx Index, filename string) error {
 	cfname := C.CString(filename)
@@ -17,18 +23,33 @@ func WriteIndex(idx Index, filename string) error {
 	return nil
 }
 
-// IO flags
-const (
-	IOFlagMmap     = C.FAISS_IO_FLAG_MMAP
-	IOFlagReadOnly = C.FAISS_IO_FLAG_READ_ONLY
-)
-
 // ReadIndex reads an index from a file.
 func ReadIndex(filename string, ioflags int) (*IndexImpl, error) {
 	cfname := C.CString(filename)
 	defer C.free(unsafe.Pointer(cfname))
 	var idx faissIndex
 	if c := C.faiss_read_index_fname(cfname, C.int(ioflags), &idx.idx); c != 0 {
+		return nil, getLastError()
+	}
+	return &IndexImpl{&idx}, nil
+}
+
+// [DIST] WriteIndex writes an index to files.
+func WriteIndexDist(idx Index, main_filename string) error {
+	cfname := C.CString(main_filename)
+	defer C.free(unsafe.Pointer(cfname))
+	if c := C.faiss_write_index_fname_dist(idx.cPtr(), cfname); c != 0 {
+		return getLastError()
+	}
+	return nil
+}
+
+// [DIST] ReadIndex reads an index from file.
+func ReadIndexDist(main_filename string, ioflags int) (*IndexImpl, error) {
+	cfname := C.CString(main_filename)
+	defer C.free(unsafe.Pointer(cfname))
+	var idx faissIndex
+	if c := C.faiss_read_index_fname_dist(cfname, C.int(ioflags), &idx.idx); c != 0 {
 		return nil, getLastError()
 	}
 	return &IndexImpl{&idx}, nil
